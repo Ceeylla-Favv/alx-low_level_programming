3c6a38e7be9bb5ffb00a64157b1e32b318864827
Ceeylla-Favv <bertleypriscy@gmail.com>
2023-04-26 14:55:13 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z', lc++)
+
+	_putchar(lc);
+	_putchar('\n');
+}
