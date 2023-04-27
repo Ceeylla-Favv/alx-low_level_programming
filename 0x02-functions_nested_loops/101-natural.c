a1b3dec754ad64d322cb9e323237ccf44f78b62c
Ceeylla-Favv <bertleypriscy@gmail.com>
2023-04-26 18:15:48 -0700

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,9 +6,8 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
+;	unsigned long int sum3, sum5, sum;
 	int i;
-
 	sum3 = 0;
 	sum5 = 0;
 	sum = 0;
@@ -19,7 +18,7 @@ int main(void)
 			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum5 = sum5 + i
+			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
