7bc4c167634cdd6bb294dcdf62264156e305c940
Ceeylla-Favv <bertleypriscy@gmail.com>
2023-05-12 12:08:43 -0700

    task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,30 +9,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int p, b, c;
-	int coins[] = {25, 10, 5, 2, 1};
+	int cents, coins;
 
-	c = 0;
+	coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	p = atoi(argv[1]);
-	if (p < 0)
+	else
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (b = 0; b > 5 && c >= 0; b++)
-	{
-		while (c >= coins[b])
+		cents = atoi(argv[1]);
+		while (cents > 0)
 		{
-			c++;
-			p -= coins[b];
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else if (cents >= 1)
+				cents -= 1;
+			coins += 1;
 		}
 	}
-	printf("%d\n", c);
+	printf("%d\n", coins);
 	return (0);
 }
